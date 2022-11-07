ee7737a98d18b19a642d82718c955a35e313ea09
Bianca Ionel <biancagionel@gmail.com>
2022-11-07 16:31:02 +0000

    mvc00

---
+++ Product.php
@@ -0,0 +1,17 @@
+<?php
+    // fichero que simula el modelo con datos
+    class Product{
+        const PRODUCTS = [
+            array(1,'Cortacesped'),
+            array(1,'Pizarra'),
+            array(1,'Billar'),
+            array(1,'Dardos')
+        ];
+    function __construct(){ /*cons vacio*/ }
+    public static function all(){
+        return Product::PRODUCTS;
+    }
+    public function find($id){
+        return Product::PRODUCTS[$id-1];
+    }
+    }
