a28a940273703a1cc1a880e66aa432b214ec4893
Bianca Ionel <biancagionel@gmail.com>
2022-11-07 15:55:57 +0000

    inicial

---
+++ public/bbdd.php
@@ -0,0 +1,19 @@
+<?php
+        require"../bbddcon.php";
+        $sql = "select nombreusu,password from credenciales";
+        $registros = $bd->query($sql);
+        echo "<br>Numero de registros devueltos: " . $registros->rowCount();
+        if($registros->rowCount() > 0){
+            foreach($registros as $fila){
+                echo "<br>Nombre de usuario : " . $fila["nombreusu"];
+                echo "<br>Password : " . $fila["password"];
+            }
+        } else {
+            echo "<br>No se ha devuelto ninguna fila";
+        }
+
+    /*
+    insertar una nueva fila en credenciales ; usuario2 : error (cifrada)
+    actualizar fila en credenciales
+    borrar fila en credenciales 
+    */
---
+++ public/index.php
@@ -0,0 +1,3 @@
+<?php
+    echo "<h2>Contenido Publico</h2>";
+    require "../start.php"; //me muevo al directorio de arriba: /var/www/html
