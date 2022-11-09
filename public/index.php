d2425066ade1f0607aff246afde398cf14251702
Bianca Ionel <biancagionel@gmail.com>
2022-11-09 16:31:02 +0000

    modificacion

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
-    echo "<h2>Contenido Publico</h2>";
+    // echo "<h2>Contenido Publico</h2>";
     require "../Controller.php"; //me muevo al directorio de arriba: /var/www/html
-    $controller = new Controller();
+    
+    /*$controller = new Controller();
     $controller->home();
+    $controller->show();
+    */
--- start.php
+++ start.php
@@ -1,2 +1,14 @@
 <?php
-    echo "<h2>Contenido PRIVADO</h2>";
+    //echo "<h2>Contenido PRIVADO</h2>";
+    // /recurso/accion/parametro
+        //recurso : controlador
+        // accion : metodos del controlador . controlador -> show()
+        // parametros : posibles parametros . find -> id de producto.
+    require_once "../Controller.php";
+
+    $app = new Controller(); 
+    //defino variable de peticion en la url 
+    if(isset($_GET["method"])){
+        $app->$_GET["method"]; //show, find, create...
+        $app->method();
+    }
